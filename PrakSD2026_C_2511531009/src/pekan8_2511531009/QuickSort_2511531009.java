73da07a06246b42d368b0eb1d5e48fcec76e9a25
HamdiSAlifi <2511531009_hamdi@student.unand.ac.id>
2026-06-01 16:31:33 +0700

    :/

--- PrakSD2026_C_2511531009/src/pekan8_2511531009/QuickSort_2511531009.java
+++ PrakSD2026_C_2511531009/src/pekan8_2511531009/QuickSort_2511531009.java
@@ -26,7 +26,7 @@ public class QuickSort_2511531009 {
 		swap_1009(arr_1009, mid_1009, high_1009);
 	}
 
-	// func :
+	// func : using median of three
 	static int partition_1009(int[] arr_1009, int low_1009, int high_1009) {
 		// calls medianOfThree method before initialising a pivot
 		medianOfThree(arr_1009, low_1009, high_1009);
@@ -42,7 +42,7 @@ public class QuickSort_2511531009 {
 				swap_1009(arr_1009, i_1009, j_1009);
 			}
 		}
-		swap_1009(arr_1009, i_1009 + 1, high_1009); // ✅ FIX 1: place pivot in correct position
+		swap_1009(arr_1009, i_1009 + 1, high_1009); 
 		return (i_1009 + 1);
 	}
 
@@ -51,13 +51,13 @@ public class QuickSort_2511531009 {
 		if (low_1009 < high_1009) {
 			int pi_1009 = partition_1009(arr_1009, low_1009, high_1009);
 			quickSort_1009(arr_1009, low_1009, pi_1009 - 1);
-			quickSort_1009(arr_1009, pi_1009 + 1, high_1009); // ✅ FIX 2: pi+1 not pi-1
+			quickSort_1009(arr_1009, pi_1009 + 1, high_1009);
 		}
 	}
 
 	public static void printArr(int[] arr_1009) {
 		for (int i_1009 = 0; i_1009 < arr_1009.length; i_1009++) {
-			System.out.print(arr_1009[i_1009] + " "); // ✅ FIX 3: err -> out
+			System.out.print(arr_1009[i_1009] + " ");
 		}
 		System.out.println();
 	}
@@ -70,7 +70,7 @@ public class QuickSort_2511531009 {
 
 		quickSort_1009(arr_1009, 0, N_1009 - 1);
 
-		System.out.print("Data Terurut quick sort: "); // ✅ FIX 4: added colon+space
+		System.out.print("Data Terurut quick sort: ");
 		printArr(arr_1009);
 	}
 }
